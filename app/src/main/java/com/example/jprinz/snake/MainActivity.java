deb049c531b28d7907fc0052c1990e31f6d195f9
jprinz <jprinz@conestogac.on.ca>
2016-06-01 12:49:53 -0400

    Added images

--- app/src/main/java/com/example/jprinz/snake/MainActivity.java
+++ app/src/main/java/com/example/jprinz/snake/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.jprinz.snake;
 
 import android.app.Activity;
+import android.graphics.Canvas;
 import android.os.Bundle;
 
 
 public class MainActivity extends Activity {
+
+    Canvas canvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
